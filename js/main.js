4fa86591ac04fd2181cbb689ef2218247928b524
mk fuentes <miguelangel.fm@outlook.com>
2020-01-13 16:01:24 -0600

    fin clase2

--- js/main.js
+++ js/main.js
@@ -1,57 +1,105 @@
-/*let button = document.getElementById('flex-btn')
-button.addEventListener('click', () => {
-    document.getElementById('square-wrapper').classList.add('d-flex')
-})*/
-/*sujeto,verbo,predicado*/
-$('#flex-btn').on('click', () => {
-    $('#square-wrapper').toggleClass("d-flex");
-    /* question ? if true : if false*/
-    $('#square-wrapper').hasClass('d-flex') ?  $('#flex-btn').text('Go Block!') : $('#flex-btn').text('Go Flex!') 
-})
-$('input[name="toggle-flex"]').on('change',(event) => {
-    let flexValue = $(event.target).val();
-    $("#square-wrapper").css({'justify-content': flexValue })
-})
-/*Escribir una tarea
-Presionar el botón para agregarla
-    debe escuchar un click
-    obtener el valor del input
-    limpiar el input
-    crear un li
-    agregamos el valor obtenido como texto del li
-    insertar en la lista*/
-var taskArray = JSON.parse(localStorage.getItem("taskArray"));
-    
-$('.to-do-wrapper .btn').on('click', () => {
-    let task = $('#task-input').val();
-    $('#task-input').val("");
-    let taskLi = `<li>${task} <div class="btn btn-danger">Eliminar</div></li>`
-    $('.task-list').append(taskLi);
-    taskArray.push(task);
-    localStorage.setItem("taskArray",JSON.stringify(taskArray))
-})
-var previousTasks = localStorage.getItem("taskArray")
-$(document).ready(()=>{
-    const printTasks = (tasks) => {
-        tasks.forEach((task)=> {
-            $('.task-list').append(
-                `<li>${task} <div class="btn btn-danger">Eliminar</div></li>`
-            )
-        })
-    }
-    
-    var previousTasks = JSON.parse(localStorage.getItem("taskArray"))
-    console.log(previousTasks);
-    previousTasks ? printTasks(previousTasks) : taskArray = [];
-})
-$(document).on('click','.btn-danger', (event) => {
-    let taskIndex  = ($(event.target).closest("li").index())
-    taskArray.splice(taskIndex,1)
-    localStorage.setItem("taskArray", JSON.stringify(taskArray))
-    $(event.target).closest("li").remove()
-    
-})
-
-/* -----
+/*$.ajax({
+    url: "https://rickandmortyapi.com/api/character",
+    data: data,
+    success: success,
+    dataType: dataType
+  });*/
+  /*
+Verbos de http
+Operaciones CRUD
+    C reate
+    R ead
+    U pdate
+    D elete
+Get : traer datos
+Post : enviar datos
+Delete : eliminar datos
+Patch : actualizar datos
 */
-
+$(document).on('click','.delete-user',(event) => {
+    let userKey = $(event.target).data("user-key");
+    console.log(userKey)
+    $.ajax({
+        url:`https://jquerycrud-ed8dc.firebaseio.com/users/${userKey}.json`,
+        method:"DELETE",
+        success:(response)=>{
+            console.log(response)
+            printUsers();
+        }
+    })
+})
+const getUsers = () => {
+    $.ajax({
+        url: "https://jquerycrud-ed8dc.firebaseio.com/users/.json",
+        method: "GET",
+        success: (response) => {
+            console.log(response)
+            console.log(typeof response)
+            $.each(response, (key, value) => {
+                $("body").append(`
+                    <p><b>User key: ${key}</b> <i>User Name: ${value.userName}</i></p>
+                `)
+            })
+        }
+    });
+}
+var userObject = {
+    userName: "Fernanda",
+    userAge: "23",
+    userPhone: "5554545455"
+}
+const patchUser = () => {
+    $.ajax({
+        url: "https://jquerycrud-ed8dc.firebaseio.com/users/-LyWw5mZ9bCv9Q-YrEUK.json",
+        method: "PATCH",
+        data: JSON.stringify({userName:"Fernanda Palacios"}),
+        success: (response) => {
+            console.log(response);
+            printUsers();
+        }
+    });
+}
+const postUser = (userData) => {
+    $.ajax({
+        url: "https://jquerycrud-ed8dc.firebaseio.com/users/.json",
+        method: "POST",
+        data: JSON.stringify(userData),
+        success: (response) => {
+            console.log(response);
+            printUsers();
+        }
+    });
+}
+const getUserData = () => {
+    let userName = $("#user-name").val()
+    let userAge = $("#user-age").val()
+    let userPhone = $("#user-phone").val()
+    let userObject = {userName, userAge, userPhone}
+    postUser(userObject);
+}
+const printUsers = () => {
+    let usersCollection = {};
+    $("#users-table tbody").empty();
+    $.ajax({
+        url:"https://jquerycrud-ed8dc.firebaseio.com/users/.json",
+        method:"GET",
+        success:(response) => {
+            usersCollection = response;
+            let i = 1;
+            $.each(usersCollection, (key, value) => {
+                $("#users-table tbody").append(`
+                <tr>
+                    <th scope="row">${i}</th>
+                    <td>${key}</td>
+                    <td>${value.userName}</td>
+                    <td>${value.userAge}</td>
+                    <td>${value.userPhone}</td>
+                    <td><div class="btn btn-danger delete-user" data-user-key=${key}>Eliminar</div></td>
+                </tr>
+                `)
+                i++
+            })
+        }
+    })
+}
+printUsers()
